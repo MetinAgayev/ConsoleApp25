b9c3d5c793eaf757c6ceafe5002cd094853f96ed
MetinAgayev <matinga@code.edu.az>
2021-12-14 01:51:25 +0400

    Add project files.

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+      public  int a;
+     public     int b;
+        public void Toplama(int a,int b)
+        {            
+            Console.WriteLine(a+b);
+        }
+        public void Cixma(int a,int b)
+        {
+            Console.WriteLine(a-b);
+        }
+        public void Vurma(int a,int b)
+        {
+            Console.WriteLine(a*b);
+        }
+        public void Bolme(int a,int b)
+        {
+            Console.WriteLine(a/b);
+        }
+    }
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Bolme(10,5);
+        }
+    }
+}
